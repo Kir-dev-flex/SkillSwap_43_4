2c3cd0909b02fadfb196570ca88b52dfc1c94063
Alex D <spiritxiii@yandex.ru>
2025-12-22 17:03:42 +0300

    fix config files

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,14 @@
+import Footer from '../widgets/footer/Footer';
+
 function App() {
   return (
     <div className='app'>
       <h1>SkillSwap Project</h1>
       <p>Стартовая страница проекта</p>
+
+      <Footer />
     </div>
   );
 }
+
 export default App;
