49868b7aeae2729ca83b51d64170d193077042af
alici <alici@DESKTOP-4T747T3>
2025-12-24 14:43:20 +0300

    added Header and Footer for pages with errors

--- src/pages/error404/Error404.tsx
+++ src/pages/error404/Error404.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Header from '../../widgets/header/Header';
+import Footer from '../../widgets/footer/Footer';
 import PrimaryButton from '../../shared/ui/button/PrimaryButton/PrimaryButton';
 import SecondaryButton from '../../shared/ui/button/SecondaryButton/SecondaryButton';
 
@@ -7,23 +9,27 @@ import Image404 from '../../images/Image404.svg';
 import styles from './Error404.module.css';
 
 const Error404: React.FC = () => (
-  <div className={styles.container}>
-    <div className={styles.image}>
-      <img src={Image404} alt='Ошибка 404' className={styles.image} />
-    </div>
+  <div>
+    <Header />
+    <div className={styles.container}>
+      <div className={styles.image}>
+        <img src={Image404} alt='Ошибка 404' className={styles.image} />
+      </div>
 
-    <div className={styles.error}>
-      <div className={styles.errorText}>
-        <h2 className={styles.title}>Страница не найдена</h2>
-        <p className={styles.description}>
-          К сожалению, эта страница недоступна. Вернитесь на главную страницу или попробуйте позже
-        </p>
-      </div>
-      <div className={styles.buttons}>
-        <SecondaryButton label='Сообщить об ошибке' className={styles.buttonItem} />
-        <PrimaryButton label='На главную' className={styles.buttonItem} />
+      <div className={styles.error}>
+        <div className={styles.errorText}>
+          <h2 className={styles.title}>Страница не найдена</h2>
+          <p className={styles.description}>
+            К сожалению, эта страница недоступна. Вернитесь на главную страницу или попробуйте позже
+          </p>
+        </div>
+        <div className={styles.buttons}>
+          <SecondaryButton label='Сообщить об ошибке' className={styles.buttonItem} />
+          <PrimaryButton label='На главную' className={styles.buttonItem} />
+        </div>
       </div>
     </div>
+    <Footer />
   </div>
 );
 
