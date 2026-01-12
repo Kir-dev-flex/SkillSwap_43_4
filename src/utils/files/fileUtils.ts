774a9321fe8139ac193e56da3bd87197da97d2fb
Alex D <spiritxiii@yandex.ru>
2026-01-12 19:46:59 +0300

    move to file calculateAge and fileToBase64

---
+++ src/utils/files/fileUtils.ts
@@ -0,0 +1,11 @@
+/**
+ * Преобразование File в base64 строку для сохранения
+ */
+
+export const fileToBase64 = (file: File): Promise<string> =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result as string);
+    reader.onerror = (error) => reject(error);
+  });
